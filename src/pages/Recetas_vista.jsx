619505d91a55b67b44f0532dab7e4bb9635a46cc
Jkro14 <joelweet@gmail.com>
2024-04-18 17:10:34 +0200

    cambios sobre como se veian las recetas.

--- src/pages/Recetas_vista.jsx
+++ src/pages/Recetas_vista.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 
 const Recetas_vista = () => {
+  fetch ('https://example.com/api/recipe')
+
   return (
     <div className="max-w-2xl mx-auto p-4">
       <header className="flex justify-between items-center mb-4">
@@ -27,11 +29,11 @@ const Recetas_vista = () => {
         <img className="w-full" src="src\assets\Recetas\Receta1.jpg" alt="Receta" />
 
         <div className="p-4">
-          <h2 className="text-lg font-bold mb-2 text-orange-500">
+          <h2 className="text-xl font-bold mb-2 text-orange-500">
             Nombre de la Receta
           </h2>
 
-          <p className="text-gray-500 mb-4">Nombre del Restaurante</p>
+          <p className="text-gray-500 mb-4 text-ml">Nombre del Restaurante</p>
 
           <div className="flex justify-between text-sm mb-4">
             <div className="flex items-center">
@@ -62,38 +64,40 @@ const Recetas_vista = () => {
             </div>
           </div>
 
-          <h3 className="font-bold mb-2 text-orange-500">Ingredientes:</h3>
+          <h3 className="font-bold mb-2 text-orange-500 text-xl">Ingredientes:</h3>
 
-          <ul className="text-gray-500">
+          <ul className="text-gray-500 text-ml">
             <li>Ingrediente 1</li>
             <li>Ingrediente 2</li>
             <li>Ingrediente 3</li>
           </ul>
 
-          <h3 className="font-bold mb-2 mt-4 text-orange-500">Preparación:</h3>
+          <h3 className="font-bold mb-2 mt-4 text-orange-500 text-xl">Preparación:</h3>
 
           <div className="flex flex-col">
             <div className="border border-gray-200 p-3 mb-4">
               <img className="w-full" src="src\assets\Recetas\paso1.avif" alt="Receta" />
-              <p className="text-sm text-gray-500">
+              <p className="text-ml text-gray-500">
                 Paso 1 de la preparación...
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem quas maiores tempore possimus vitae, atque, molestias sequi doloribus non fugiat aut hic facere ut? Animi sunt labore fugiat velit esse.
               </p>
             </div>
             <div className="border border-gray-200 p-3 mb-4">
-              <img className="w-full" src="src\assets\Recetas\paso1.avif" alt="Receta" />
-              <p className="text-sm text-gray-500">
+              <img className="w-full" src="src\assets\Recetas\paso2.jpg" alt="Receta" />
+              <p className="text-ml text-gray-500">
                 Paso 2 de la preparación...
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quasi suscipit distinctio dolore dolorem quas ea perferendis placeat optio molestiae eos quia ipsam dolorum aspernatur, culpa doloremque voluptates dignissimos rerum!
               </p>
             </div>
           </div>
 
           <div className="flex justify-between items-center">
-            <a href="#" className="text-blue-500 hover:underline">
+            <button href="/" className="text-blue-500">
               Ver en Instagram
-            </a>
-            <a href="#" className="text-blue-500 hover:underline">
+            </button>
+            <button href="/" className="text-blue-500">
               Visitar Restaurante
-            </a>
+            </button>
           </div>
         </div>
       </div>
