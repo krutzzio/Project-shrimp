420e4527f26d9fb05c2fd5127f5bf39752ccca12
raquel_martos <raquel.martos.jover@gmail.com>
2024-04-18 16:49:14 +0200

    componente navbar

--- src/pages/HomeRecetas.jsx
+++ src/pages/HomeRecetas.jsx
@@ -1,5 +1,3 @@
-import logoGamba from "../assets/logo/logoGamba_logoNaranja.svg";
-import iconoGamba from "../assets/logo/iconoGamba_iconoNaranja.svg";
 import {
   Avatar,
   Button,
@@ -7,14 +5,7 @@ import {
   DropdownItem,
   DropdownMenu,
   DropdownTrigger,
-  Input,
-  Switch,
 } from "@nextui-org/react";
-import SwitchHome from "../components/SwitchHome";
-import lupa from "../assets/iconos/iconos_Lupa.svg";
-import CP from "../assets/iconos/iconos_Mapa.svg";
-import receta from "../assets/iconos/iconos_Recetas.svg";
-import rest from "../assets/iconos/iconos_Restaurante.svg";
 import mex from "../assets/iconosComida/Taco.svg";
 import chino from "../assets/iconosComida/Chino.svg";
 import burguer from "../assets/iconosComida/Hamburguesa.svg";
@@ -26,6 +17,7 @@ import italia from "../assets/iconosComida/Pizza.svg";
 import poke from "../assets/iconosComida/Poke.svg";
 import { Link } from "react-router-dom";
 import React from "react";
+import NavBar from "../components/navbar/NavBar";
 
 export default function HomeRecetas() {
   const [selectedKeys, setSelectedKeys] = React.useState(new Set(["Dietética"]));
@@ -41,104 +33,8 @@ export default function HomeRecetas() {
   );
 
   return (
-    <div>
-      <header className="relative z-10 flex p-4 items-center justify-between lg:px-8">
-        {/*Desktop*/}
-
-        {/*Logo*/}
-        <img
-          className="hidden w-32 lg:block lg:w-44"
-          src={logoGamba}
-          alt="Gamba Logo"
-        />
-        {/*Switch largo*/}
-        <section className="hidden mx-2 lg:block">
-          <SwitchHome />
-        </section>
-        {/*Codigo Postal*/}
-        <section className="hidden lg:block w-24">
-          <Input
-            variant="underlined"
-            classNames={{
-              input: [
-                "bg-transparent",
-                "text-black/90 dark:text-white/90",
-                "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-              ],
-              innerWrapper: "bg-transparent",
-              inputWrapper: [
-                "group-data-[focused=true]:bg-default-200/50",
-                "!cursor-text",
-              ],
-            }}
-            placeholder="CP..."
-            startContent={<img src={CP} className="w-5" />}
-          />
-        </section>
-        {/*Barra Buscador*/}
-        <section className="hidden mx-2 lg:block w-1/2">
-          <Input
-            variant="underlined"
-            classNames={{
-              input: [
-                "bg-transparent",
-                "text-black/90 dark:text-white/90",
-                "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-              ],
-              innerWrapper: "bg-transparent",
-              inputWrapper: [
-                "group-data-[focused=true]:bg-default-200/50",
-                "!cursor-text",
-              ],
-            }}
-            placeholder="Buscar..."
-            startContent={<img src={lupa} className="w-5" />}
-          />
-        </section>
-
-        {/*Mobile*/}
-
-        {/*Logo + switch pequeño*/}
-        <div className="flex gap-2 items-center">
-          <img
-            className="hidden w-32 md:block lg:hidden lg:w-44"
-            src={logoGamba}
-            alt="Gamba Logo"
-          />
-          <img
-            className="w-9 md:hidden lg:w-44"
-            src={iconoGamba}
-            alt="Gamba Icono"
-          />
-          <Switch
-            className="lg:hidden"
-            defaultSelected
-            size="lg"
-            color="primary"
-            thumbIcon={({ isSelected }) =>
-              isSelected ? (
-                <img src={rest} className="w-5" />
-              ) : (
-                <img src={receta} className="w-4" />
-              )
-            }
-          />
-        </div>
-        {/*iconos + avatar*/}
-        <div className="flex gap-2 items-center">
-          <button className="lg:hidden w-8">
-            <img src={CP} />
-          </button>
-          <button type="button" className="lg:hidden w-8">
-            <img src={lupa} />
-          </button>
-          <Avatar
-            size="sm"
-            classNames={{ base: "bg-[#FE7139]", icon: "text-[#F2F2F2]" }}
-          />
-        </div>
-      </header>
-
+    <div className="max-w-screen-2xl mx-auto">
+      <NavBar/>
       <main className="block">
         <div className="pl-4 lg:pl-8">
           {/*Tipos de comida*/}
