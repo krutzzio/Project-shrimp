7c10c62376c87db3d7ccfcb3173700902d7a40af
Jkro14 <joelweet@gmail.com>
2024-04-24 18:33:32 +0200

    cambios de Busqueda

--- src/pages/Busqueda.jsx
+++ src/pages/Busqueda.jsx
@@ -1,9 +1,33 @@
 import { useState, useEffect } from "react";
+import SwitchHome from "../components/SwitchHome";
+import {
+  Avatar,
+  Button,
+  Checkbox,
+  CheckboxGroup,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Switch,
+  useDisclosure,
+} from "@nextui-org/react";
+import logoGamba from "../assets/logo/logoGamba_logoNaranja.svg";
+import iconoGamba from "../assets/logo/iconoGamba_iconoNaranja.svg";
+import lupa from "../assets/iconos/iconos_Lupa.svg";
+import CP from "../assets/iconos/iconos_Mapa.svg";
+import rest from "../assets/iconos/iconos_Restaurante.svg";
+import receta from "../assets/iconos/iconos_Recetas.svg";
+import filtro from "../assets/iconos/iconos_Filtro.svg";
+import { Link } from "react-router-dom";
 
 const Busqueda = () => {
   const [users, setUsers] = useState([]);
   const [search, setSearch] = useState("");
   const [filter, setFilter] = useState("all");
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   const URL = "https://jsonplaceholder.typicode.com/photos";
 
@@ -23,38 +47,119 @@ const Busqueda = () => {
 
   const handleFilter = (e) => {
     setFilter(e.target.value);
-  }
-  
+  };
+
   let filteredUsers = users;
-  
+
   if (filter !== "all") {
-    filteredUsers = filteredUsers.filter(user => {
+    filteredUsers = filteredUsers.filter((user) => {
       return user.title.toLowerCase().includes(filter.toLowerCase());
     });
   }
 
   if (search) {
-    filteredUsers = filteredUsers.filter(user => {
+    filteredUsers = filteredUsers.filter((user) => {
       return user.title.toLowerCase().includes(search.toLowerCase());
     });
   }
 
   return (
-    <div className="bg-orange-500 p-4">
-      <input 
-        className="w-full p-2 border border-orange-400 rounded mb-4"
-        type="text" 
-        placeholder="Buscar..."
-        value={search}
-        onChange={handleSearch}  
-      />
-      
-      <select className="w-full p-2 border border-orange-400 rounded mb-4" onChange={handleFilter}>
-        <option value="all">Todos</option>
-        <option value="qui">qui</option>
-        <option value="est">est</option>
-        <option value="odio">odio</option>
-      </select>
+    <main className="w-full p-2">
+      <section className="relative z-10 flex flex-row items-center justify-between lg:gap-4">
+        {/*Mobile*/}
+
+        {/*Logo + switch pequeño*/}
+        <div className="flex gap-2 items-center">
+          <Link to={"/home"} className="w-9 md:hidden lg:w-44">
+            <img src={iconoGamba} alt="Gamba Icono" />
+          </Link>
+          <Switch
+            className="lg:hidden"
+            defaultSelected
+            size="lg"
+            color="primary"
+            thumbIcon={({ isSelected }) =>
+              isSelected ? (
+                <img src={rest} className="w-5" />
+              ) : (
+                <img src={receta} className="w-4" />
+              )
+            }
+          />
+        </div>
+
+        {/*iconos + avatar*/}
+        <div className="flex gap-2 items-center">
+          <button className="lg:hidden w-8">
+            <img src={CP} />
+          </button>
+          <Link to={"/perfil"}>
+            <Avatar
+              size="md"
+              classNames={{ base: "bg-[#FE7139]", icon: "text-[#F2F2F2]" }}
+            />
+          </Link>
+        </div>
+      </section>
+
+      <div className="flex items-center justify-between mt-4 lg:hidden">
+        <Input
+          variant="faded"
+          radius="full"
+          placeholder="Buscar..."
+          startContent={<img src={lupa} className="w-6" />}
+          value={search}
+          onChange={handleSearch}
+        />
+        <img className="w-10" onClick={onOpen} src={filtro} />
+      </div>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Filtros</ModalHeader>
+              <ModalBody>
+                <CheckboxGroup
+                  label="Dietética"
+                  orientation="vertical"
+                  color="primary"
+                  
+                >
+                  <Checkbox value="vegetariano">Vegetariano</Checkbox>
+                  <Checkbox value="vegano">Vegano</Checkbox>
+                </CheckboxGroup>
+                <CheckboxGroup
+                  label="Dificultad"
+                  orientation="vertical"
+                  color="primary"
+                >
+                  <Checkbox value="facil">Fácil</Checkbox>
+                  <Checkbox value="media">Media</Checkbox>
+                  <Checkbox value="difícil">Difícil</Checkbox>
+                </CheckboxGroup>
+                <CheckboxGroup
+                  label="Plato"
+                  orientation="vertical"
+                  color="primary"
+                >
+                  <Checkbox value="entrante">Entrante</Checkbox>
+                  <Checkbox value="primero">Primero</Checkbox>
+                  <Checkbox value="segundo">Segundo</Checkbox>
+                  <Checkbox value="postre">Postre</Checkbox>
+                </CheckboxGroup>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Cerrar
+                </Button>
+                <Button color="primary" onPress={onClose} >
+                  Aplicar
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
 
       <table className="w-full bg-white rounded-lg overflow-hidden shadow">
         <thead className="bg-orange-600 text-white">
@@ -63,18 +168,24 @@ const Busqueda = () => {
           </tr>
         </thead>
         <tbody>
-          {filteredUsers.map(user => (
-            <tr key={user.id} className="border-b border-orange-200 hover:bg-orange-100">
+          {filteredUsers.map((user) => (
+            <tr
+              key={user.id}
+              className="border-b border-orange-200 hover:bg-orange-100"
+            >
               <td className="p-3">
-                <img className="mx-auto block" src={user.thumbnailUrl} alt={user.title}/>
+                <img
+                  className="mx-auto block"
+                  src={user.thumbnailUrl}
+                  alt={user.title}
+                />
                 <p className="text-center capitalize ">{user.title}</p>
-                
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-    </div>
+    </main>
   );
 };
 
