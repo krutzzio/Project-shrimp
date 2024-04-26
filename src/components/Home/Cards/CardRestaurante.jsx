4610aa8642722aa23d778073a9986782abe6b01d
raquel_martos <raquel.martos.jover@gmail.com>
2024-04-26 14:48:51 +0200

    card recetas y restaurante

--- src/components/Home/Cards/CardRestaurante.jsx
+++ src/components/Home/Cards/CardRestaurante.jsx
@@ -1,4 +1,4 @@
-import { Card, CardFooter, Chip, Image } from "@nextui-org/react";
+import { Card, CardFooter, Chip } from "@nextui-org/react";
 import restaurante1 from "../../../assets/perfilRestaurante/perfilRestaurante1.jpg";
 import fav from "../../../assets/iconos/iconos_Favorito.svg";
 import { Link } from "react-router-dom";
@@ -7,35 +7,32 @@ export function CardRestaurante() {
 
     return (
 
-        <Link className="max-w-fit min-w-50 items-center flex flex-col">
+        <Link to={"/perfil/restaurante-cliente"} className="max-w-fit min-w-50 items-center flex flex-col">
             <div className="flex flex-col gap-1 relative shadow-lg my-4 rounded-2xl">
-            <Card
-                isFooterBlurred
-                className="border-none"
-            >
-                <Chip color="primary" className="absolute z-20 top-[0.5rem] left-[0.5rem] text-white">Italiano</Chip>
-                <Image
-                    alt="Foto del restaurante"
-                    width={250}
-                    height={200}
-                    radius="none"
-                    className="object-cover h-72"
-                    src={restaurante1}
-                />
-                <CardFooter
-                    className="justify-between before:bg-white/10 overflow-hidden py-4 absolute before:rounded-xl rounded-large bottom-2 w-[calc(100%_-_8px)] shadow-small ml-1 z-10"
+                <Card
+                    isFooterBlurred
+                    className="border-none"
                 >
-                    <div className="flex absolute z-11 gap-2 items-center">
-                        <div className="flex flex-col leading-3">
-                            <p className="text-xl font-bold text-white leading-6">Don Kamaron</p>
-                            <p className="text-md text-white">Barcelona</p>
+                    <Chip color="primary" className="absolute z-20 top-[0.5rem] left-[0.5rem] text-white">Italiano</Chip>
+                    <img
+                        alt="Foto del restaurante"
+                        className="object-cover w-52 h-64"
+                        src={restaurante1}
+                    />
+                    <CardFooter
+                        className="justify-between before:bg-white/10 overflow-hidden py-4 absolute before:rounded-xl rounded-large bottom-2 w-[calc(100%_-_8px)] shadow-small ml-1 z-10"
+                    >
+                        <div className="flex absolute z-11 gap-2 items-center">
+                            <div className="flex flex-col leading-4">
+                                <p className="text-xl font-bold text-white leading-6">Don Kamaron</p>
+                                <p className="text-md text-white">Barcelona</p>
+                            </div>
                         </div>
-                    </div>
-                    <Link>
-                        <img className="h-6" src={fav} alt="Guardar en favoritos" />
-                    </Link>
-                </CardFooter>
-            </Card>
+                        <Link>
+                            <img className="h-6 items-start" src={fav} alt="Guardar en favoritos" />
+                        </Link>
+                    </CardFooter>
+                </Card>
             </div>
         </Link>
 
