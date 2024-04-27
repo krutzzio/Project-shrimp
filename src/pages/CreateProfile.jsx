f132ab75478d45c8aa12e113ae8569ef08ec8da1
raquel_martos <raquel.martos.jover@gmail.com>
2024-04-27 11:29:47 +0200

    blobs formulari

--- src/pages/CreateProfile.jsx
+++ src/pages/CreateProfile.jsx
@@ -8,6 +8,9 @@ import CreateProfileRest from "../components/CreateProfile/Restaurant/CreateProf
 import CreateProfileClient from "../components/CreateProfile/Client/CreateProfileClient"
 import { RegisterContext } from "../contexts/RegisterContext"
 import { registroCliente, registroRest } from "../utils/fetchs/registros"
+import shape1 from "../assets/shapes/shape1.svg";
+import shape2 from "../assets/shapes/shape2.svg";
+import shape3 from "../assets/shapes/shape3.svg";
 
 
 export default function CreateProfile() {
@@ -70,10 +73,10 @@ export default function CreateProfile() {
             </Button>
             ) */
           <section className="flex gap-2">
-            <Button onClick={() => setStep(step - 1)} className={`${step === 1 && `hidden`} w-40 md:w-44 h-12 text-lg bg-primary text-[#FAFAFA] font-semibold`}>
+            <Button onClick={() => setStep(step - 1)} className={`${step === 1 && `hidden`} relative z-10 w-40 md:w-44 h-12 text-lg bg-primary text-[#FAFAFA] font-semibold`}>
               <h1>Volver</h1>
             </Button>
-            <Button onClick={register} isLoading={step > maxSteps ? true : false} className={`${step === 1 ? `w-72` : `w-40`} md:w-44 h-12 text-lg bg-primary text-[#FAFAFA] font-semibold`}>
+            <Button onClick={register} isLoading={step > maxSteps ? true : false} className={`${step === 1 ? `w-72` : `w-40`} relative z-10 md:w-44 h-12 text-lg bg-primary text-[#FAFAFA] font-semibold`}>
               {
                 step >= maxSteps
                   ? <h1>Completar Alta</h1>
@@ -83,6 +86,38 @@ export default function CreateProfile() {
           </section>
         }
       </footer>
+      
+      {/* Mancha amarilla izquierda */}
+      <article
+        className={`absolute z-0 bottom-[-46rem] left-[-20rem] rotate-[200deg] md:left-[-30rem] md:bottom-[0rem]`
+          }
+      >
+        <img src={shape2} className="max-w-[60rem] w-[60rem]" alt="" />
+      </article>
+
+      {/* Mancha amarilla derecha */}
+      <article
+        className={`absolute z-0 top-[-35rem] right-[-45rem] rotate-[300deg] md:right-[-30rem] md:top-[-10rem]`
+        }
+      >
+        <img src={shape1} className="max-w-[60rem] w-[60rem]" alt="" />
+      </article>
+
+      {/* Mancha naranja derecha */}
+      <article
+        className={`absolute z-0 bottom-[-8rem] right-[-30rem] rotate-[300deg] md:right-[0rem] md:bottom-[8rem]`
+        }
+      >
+        <img src={shape3} className="max-w-[30rem] w-[30rem]" alt="" />
+      </article>
+
+      {/* Mancha naranja abajo */}
+      <article
+        className={`absolute z-0 bottom-[-2rem] left-[-38rem] rotate-[100deg] md:left-[-5rem] md:bottom-[-25rem]`
+        }
+      >
+        <img src={shape3} className="max-w-[50rem] w-[50rem]" alt="" />
+      </article>
     </div >
   )
 }
