51518c3b2e4c4defedb351e5910da4be390415f5
HannibalRD <hannibal.rabasso11@gmail.com>
2024-04-28 20:15:09 +0200

    Validacion register

--- src/components/forms/Register/RegisterForm.jsx
+++ src/components/forms/Register/RegisterForm.jsx
@@ -1,9 +1,11 @@
 import { Button } from "@nextui-org/react"
 import "../forms.css"
-import { NavLink, redirect } from "react-router-dom"
-import { useContext, useState } from "react"
+import { NavLink} from "react-router-dom"
+import { useContext, useState, useEffect } from "react"
 import { ThemeContext } from "../../../contexts/ThemeContext"
 import { RegisterContext, } from "../../../contexts/RegisterContext"
+
+
 
 export default function RegisterForm() {
 
@@ -12,7 +14,19 @@ export default function RegisterForm() {
     const [infoUser, setInfoUser] = useState(userType ? { ...rest } : { ...client })
     const [password, setPassword] = useState()
     const [passwordConfirmar, setPasswordComfirmar] = useState()
+    const [email, setEmail] = useState()
     const [error, setError] = useState()
+    const [redirectTo, setRedirectTo] = useState(null);
+
+   
+
+    useEffect(() => {
+        // Si redirectTo es true, activa el NavLink automáticamente
+        if (redirectTo) {
+          document.getElementById("navLink").click();
+        }
+      }, [redirectTo]);
+
     const handleChange = (event) => {
         const { name, value } = event.target
 
@@ -22,25 +36,37 @@ export default function RegisterForm() {
         } else if (name === 'confirmedPassword') {
             setPasswordComfirmar(value);
             console.log(passwordConfirmar)
-        } else {
+        } 
+         else {
             setInfoUser({ ...infoUser, [name]: value });
         }
     }
 
 
     const handleContinue = () => {
-        if (password === passwordConfirmar && password !== undefined) {
-            if (userType) {
-                console.log("hola")
-                setRegisterRest(infoUser);
-            } else {
-                setRegisterClient(infoUser);
-            }
-            window.location.href = `/create/${userType ? 'restaurant' : 'client'}`// se que es un mierda el windows location mañana lo cambio pero tengo suenño surry :)
-        } else {
-            setError("Las contraseñas no coinciden");
+        const emailRegex = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/
+        if (!emailRegex.test(infoUser.correo)) {
+            setError("Por favor, introduce un correo electrónico válido.");
+            return;
         }
-    };
+    
+        else if (password === passwordConfirmar && password !== undefined) {
+          if (userType) {
+            setRegisterRest(infoUser);
+          } else {
+            setRegisterClient(infoUser);
+          }
+          
+          console.log(userType)
+          setRedirectTo(true);
+        } else {
+          setError("Las contraseñas no coinciden");
+          return;
+        }
+      };
+    
+
+    
 
     return (
         <div className="w-[22rem] flex flex-col items-center m-auto gap-4">
@@ -63,8 +89,8 @@ export default function RegisterForm() {
             </article>
             <article className="w-10/12 py-8">
                 <Button className="relative z-10 w-full h-12 text-lg bg-[#272D2F] text-[#FAFAFA] font-semibold shadow-lg">
-                    <NavLink  onClick={handleContinue} 
-         className="w-full">Crear Cuenta</NavLink>
+                    <NavLink id="navLink"  to={redirectTo ? `/create/${userType ? 'restaurant' : 'client'}` : '#'} onClick={handleContinue} 
+        className="w-full">Crear Cuenta</NavLink>
                 </Button>
             </article>
         </div >
