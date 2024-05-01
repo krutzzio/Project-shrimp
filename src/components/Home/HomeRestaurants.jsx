2578c584a3af0f983dd94a7fe46c51b8278c0dc6
Krutzzio <39598943+Krutzzio@users.noreply.github.com>
2024-05-02 01:00:33 +0200

    push

--- src/components/Home/HomeRestaurants.jsx
+++ src/components/Home/HomeRestaurants.jsx
@@ -60,29 +60,7 @@ export default function HomeRestaurantes() {
                     </div>
                 </div>
             </div>
-            <div className='flex flex-col font-bold'>
-                <div className="flex items-end justify-between">
-                    <h1 className="text-2xl font-bold ml-2">Prueba algo nuevo</h1>
-                    <div className="hidden md:flex md:gap-1">
-                        <button type="button" data-carousel-prev>
-                            <img src={flechaizq} className="w-8" />
-                        </button>
-                        <button type="button" data-carousel-next>
-                            <img src={flechader} className="w-8" />
-                        </button>
-                    </div>
-                </div>
-                <div className="block overflow-x-scroll whitespace-nowrap scrollbar-hide gap-4">
-                    <div className="relative flex gap-4 ml-2">
-                        {
-                            /*  !recetasNuevas.length
-                                 ? <CardRecetaLoading />
-                                 : recetasNuevas?.map(receta => <CardReceta key={receta.receta.id} recetaInfo={receta} />)
-  */
-                        }
-                    </div>
-                </div>
-            </div>
+
         </div>
     )
 }
